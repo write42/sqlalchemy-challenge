cb22bf820cfffec76f0e653bdb143bff101f8f7e
Jade <jadeve.jimenez@yahoo.com>
2021-03-15 15:36:50 -0700

    need help with flask

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from flask import Flask
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+@app.route("/api/v1.0/precipitation")
+def rain():
+    return jsonify()
+
+@app.route("/api/v1.0/stations")
+def station():
+    return jsonify()
+
+@app.route("/api/v1.0/tobs")
+def temp():
+    return jsonify()
+
+@app.route("/api/v1.0/<start>")
+def start():
+
+@app.route("/api/v1.0/<start>/<end>")
+def end():
+
 if __name__ == "__main__":
     app.run(debug=True)    
