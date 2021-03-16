e574117df3e070bf8a30e9307a4463c9b4704fcc
Jade <jadeve.jimenez@yahoo.com>
2021-03-15 18:26:31 -0700

    copy queries from jupyter notebook

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
 from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from sqlalchemy import desc
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+conn = engine.connect()
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+session = Session(engine)
+
+inspector = inspect(engine)
+
+df = pd.read_sql("SELECT date,prcp FROM measurement WHERE date > '2016-08-23'",conn)
+df_next = df.dropna().sort_values("date")
+
+all_rain=[]
+
+for date,prcp in df_next:
+    rain_dict={}
+    rain_dict["date"] = date
+    rain_dict["prcp"] = prcp
+    all_rain.append(rain_dict)
 
 app = Flask(__name__)
 
@@ -14,7 +44,7 @@ def home():
     )
 @app.route("/api/v1.0/precipitation")
 def rain():
-    return jsonify()
+    return jsonify(all_rain)
 
 @app.route("/api/v1.0/stations")
 def station():
